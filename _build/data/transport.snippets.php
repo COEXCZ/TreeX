37e9fa040cadbde7d8e64e8efd720d4e2404a986
Jan Peca <theboxercz@gmail.com>
2013-10-11 15:41:31 +0200

    Build script

---
+++ _build/data/transport.snippets.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Add snippets to build
+ * 
+ * @package treex
+ * @subpackage build
+ */
+$snippets = array();
+
+$snippets[0]= $modx->newObject('modSnippet');
+$snippets[0]->fromArray(array(
+                            'id' => 0,
+                            'name' => 'fetchResource',
+                            'description' => 'Fetch resource data',
+                            'snippet' => getSnippetContent($sources['snippets'].'fetchresource.snippet.php'),
+                        ),'',true,true);
+
+$snippets[1]= $modx->newObject('modSnippet');
+$snippets[1]->fromArray(array(
+                             'id' => 1,
+                             'name' => 'updateResource',
+                             'description' => 'Update resource',
+                             'snippet' => getSnippetContent($sources['snippets'].'updateresource.snippet.php'),
+                        ),'',true,true);
+
+
+
+return $snippets;
