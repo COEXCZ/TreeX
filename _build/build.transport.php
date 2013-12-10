74878975da2744441ac822bad9df6853a2cbc83a
Jan Peca <pecajan@gmail.com>
2013-12-10 14:03:50 +0100

    Build script updated

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','TreeX');
 define('PKG_NAME_LOWER','treex');
-define('PKG_VERSION','1.0.16');
+define('PKG_VERSION','1.1.0');
 define('PKG_RELEASE','alpha');
 
 /* define sources */
