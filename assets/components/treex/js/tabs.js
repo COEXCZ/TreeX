8dc3823e31a81b435b518be301e9b62974db8374
Roman Klos <roman.klos@coex.cz>
2013-12-18 10:17:10 +0100

    Fixed bug with no tabs in redactor, now they are created; Added comments to code

--- assets/components/treex/js/tabs.js
+++ assets/components/treex/js/tabs.js
@@ -1,27 +1,43 @@
 if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
 
+/*
+    Redactor Tabs Management plugin for TTU
+
+    init                        Inicialization of plugin
+    buttonsInit                 Button init in modal window
+    saveTitleButtonInit         Save button init in modal window
+    saveButtonInit              Modal Save button init
+    sortableInit                Init sortable plugin
+    addButtonInit               Add button init for new tabs
+    newTab                      New tab function
+*/
+
 RedactorPlugins.tabs = {
 
     init: function()
     {
-        var _selfTabs = this;
+        var _selfTabs = this; // Whole redactor variable
 
         var callback = function() {
-            var editorHTML = $('.redactor_editor');
+            var editorHTML = $('.redactor_editor #tabs');
 
             var tabList = editorHTML.find('ul').find('li');
             var tabListHtml = $('<div />');
 
+            // Find all tabs in redactor editor
             $(tabList).each(function() {
                 var _self = $(this).find('a');
 
                 var item = { href: _self.attr('href'), title: _self.text() };
 
+                // Append new tab to the modal
                 tabListHtml.append(_selfTabs.newTab(item));
             });
 
+            // Add all items to modal
             $('#redactor_modal_inner section.tabs-content').html(tabListHtml);
 
+            // Init all buttons and sortable plugin
             _selfTabs.addButtonInit();
             _selfTabs.buttonsInit();
             _selfTabs.saveButtonInit();
@@ -29,6 +45,7 @@ RedactorPlugins.tabs = {
 
         };
 
+        // Add button and init to redactor toolbar
         _selfTabs.buttonAdd('tabs', 'Tabs Management', function()
         {
             _selfTabs.modalInit('Tabs Management', '#tabs_modal', 500, callback);
@@ -41,6 +58,7 @@ RedactorPlugins.tabs = {
     buttonsInit: function() {
         var _selfTabs = this;
 
+        // Init remove button
         $('.js-tabs-remove').on('click', function(event) {
             var _self = $(this);
 
@@ -49,14 +67,17 @@ RedactorPlugins.tabs = {
             event.preventDefault();
         });
 
+        // Init edit button
         $('.js-tabs-edit').on('click', function(event) {
             var _self = $(this),
                 parent = _self.parents('.js-tabs-item'),
                 text = parent.find('.tabs-text');
 
             parent.find('.js-tabs-buttons').hide();
+            // Add input with text and save button for edit name of tab
             text.html('<input type="text" value="' + text.text() + '" class="js-input-title" /><button type="submit" class="js-tabs-title-save">Save</button>');
 
+            // Init save button for edit
             _selfTabs.saveTitleButtonInit();
 
             event.preventDefault();
@@ -70,6 +91,7 @@ RedactorPlugins.tabs = {
                 text = parent.find('.tabs-text'),
                 value = parent.find('.js-input-title').val();
 
+            // Replace old title with new title
             parent.find('.js-tabs-buttons').show();
             text.text(value);
 
@@ -83,35 +105,52 @@ RedactorPlugins.tabs = {
         $('.js-tabs-save').on('click', function(event) {
             var tabs = $('<ul />');
 
-            $('.js-tabs-item').each(function() {
-                var _self = $(this);
-                var div = $('<div />');
-
-                if(!_self.hasClass('removed')) {
-                    tabs.append('<li><a href="' + _self.data('href') + '">' + _self.find('.tabs-text').text() + '</a></li>');
-
-                    if($('div' + _self.data('href')).length === 0) {
-                        var id = _self.data('href');
-
-                        if (id.substring(0, 1) == '#') {
-                          id = id.substring(1);
-                        }
-                        var container = div.attr('id', id).text('Add text');
-                        $('.redactor_editor #tabs').append(container);
-                    }
-                } else {
-                    if($('div' + _self.data('href')).length !== 0) {
-                        $('div' + _self.data('href')).remove();
-                    }
+            // If there are no items then remove the tabs div
+            if($('.js-tabs-item').length > 0) {
+                // Check if the tabs div is created
+                if($('.redactor_editor #tabs').length === 0) {
+                    $('.redactor_editor').append($('<div id="tabs"><ul></ul></div>'));
                 }
 
-            });
+                // Iterate all items in modal window
+                $('.js-tabs-item').each(function() {
+                    var _self = $(this);
+                    var div = $('<div />');
 
-            $('.redactor_editor #tabs>ul').replaceWith(tabs);
+                    // Remove items if there are no longer in the list
+                    if(!_self.hasClass('removed')) {
+                        // Create list item
+                        tabs.append('<li><a href="' + _self.data('href') + '">' + _self.find('.tabs-text').text() + '</a></li>');
 
+                        // Does it already exist in editor if not then create new div with default text
+                        if($('div' + _self.data('href')).length === 0) {
+                            var id = _self.data('href');
+
+                            if (id.substring(0, 1) == '#') {
+                              id = id.substring(1);
+                            }
+                            var container = div.attr('id', id).text('Add text');
+                            $('.redactor_editor #tabs').append(container);
+                        }
+                    } else {
+                        if($('div' + _self.data('href')).length !== 0) {
+                            $('div' + _self.data('href')).remove();
+                        }
+                    }
+
+                });
+
+                // Replace all tabs
+                $('.redactor_editor #tabs>ul').replaceWith(tabs);
+            } else {
+                $('.redactor_editor #tabs').remove();
+            }
+
+            // Sync editor with textarea
             _selfTabs.sync();
             initTabs();
 
+            // Close the modal window
             _selfTabs.modalClose();
 
             event.preventDefault();
@@ -119,6 +158,7 @@ RedactorPlugins.tabs = {
 
     },
     sortableInit: function() {
+        // Init jQuery UI Plugin Sortable
         $("#redactor_modal_inner section.tabs-content > div").sortable({
             axis: "y"
         });
@@ -126,12 +166,16 @@ RedactorPlugins.tabs = {
     addButtonInit: function() {
         var _selfTabs = this;
 
+        // Add new tab
         $('.js-tabs-add').on('click', function(event) {
             var timestamp = new Date().getTime();
+            // Create tab with timestape and title
             var item = _selfTabs.newTab({href: '#tab-' + timestamp, title: 'Tab'});
 
+            // Append to modal list
             $('#redactor_modal_inner section.tabs-content > div').append(item);
 
+            // And init all the buttons for that item
             _selfTabs.buttonsInit();
             _selfTabs.sortableInit();
 
@@ -141,8 +185,9 @@ RedactorPlugins.tabs = {
     newTab: function(item) {
         var _self = item;
 
+        // New item for the modal list item
         var item = '<div data-href="' + _self.href + '" class="js-tabs-item"><span class="tabs-text">' + _self.title + '</span> <span class="js-tabs-buttons"><a href="" class="js-tabs-edit">Edit</a> <a href="" class="js-tabs-remove">Remove</a></span></div>';
 
         return item;
     }
-}
+};
