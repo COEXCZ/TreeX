a060337ec22a307d72bae05845fa3a6d6566bf6c
Jan Peca <pecajan@gmail.com>
2014-02-17 16:04:40 +0100

    Fixed checkpolicy's snippet content. Build script updated

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','TreeX');
 define('PKG_NAME_LOWER','treex');
-define('PKG_VERSION','1.4.0');
+define('PKG_VERSION','1.4.1');
 define('PKG_RELEASE','alpha');
 
 /* define sources */
--- core/components/treex/elements/snippets/treexcheckpolicy.snippet.php
+++ core/components/treex/elements/snippets/treexcheckpolicy.snippet.php
@@ -1,49 +1,16 @@
 <?php
 /**
- * createResource
- *
- * DESCRIPTION
- *
- * Custom hook for FormIt that create a resource
- *
- * USAGE:
- *
- * [[!FormIt? &hooks=`createResource`]]
+ * TreeXCheckPolicy
  *
  */
-$corePath = $modx->getOption('treex.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/treex/');
-/** @var TreeX $treeX */
-$treeX = $modx->getService(
-    'treex',
-    'TreeX',
-    $corePath . 'model/treex/',
-    array(
-        'core_path' => $corePath
-    )
-);
 
-$values = $hook->getValues();
+if (!isset($_GET['resource'])) return;
 
-$values['published'] = isset($values['published']) ? 1 : 0;
-$values['content'] = $_POST['content'];
+/** @var modResource $resource */
+$resource = $modx->getObject('modResource', intval($_GET['resource']));
 
-$processorResponse = $modx->runProcessor('resource/create', $values);
-$response = $processorResponse->getResponse();
-
-$updateId = $modx->getOption('treex.update_form_id', null, 1);
-$nodePath = $treeX->getNodePath($response['object']['id'], $response['object']['context_key'], false);
-
-$removed = $modx->cacheManager->delete($nodePath);
-if(!$removed) {
-    $nodePathItems = explode('/', $nodePath);
-    array_pop($nodePathItems);
-
-    $nodePathItems = implode('/', $nodePathItems);
-    $nodePathItems = substr($nodePathItems, 0, -4);
-
-    $modx->cacheManager->delete($nodePathItems);
+if ($resource) {
+    return $resource->checkPolicy('save');
 }
 
-$modx->sendRedirect($modx->makeUrl($updateId, '', array('resource' => $response['object']['id'])));
-
-return true;
+return;
