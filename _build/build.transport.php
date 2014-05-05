f9579d133d16b37f4cde00329666608820858b59
Kamil Vasica <kamil.vasica@coex.cz>
2014-05-05 16:10:55 +0200

    transport package regenerated

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','TreeX');
 define('PKG_NAME_LOWER','treex');
-define('PKG_VERSION','1.4.3');
+define('PKG_VERSION','1.4.4');
 define('PKG_RELEASE','alpha');
 
 /* define sources */
